c647f6d709067e8a2c8cc8200e729926cbb4b44c
Yevgeniy Brikman <brikis98@gmail.com>
2011-05-09 11:37:21 -0700

    Package JS files with jammit. Added Google Analytics

--- config.rb
+++ config.rb
@@ -6,6 +6,7 @@ css_dir = "public/css"
 sass_dir = "sass"
 images_dir = "public/images"
 javascripts_dir = "public/js"
+output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
---
+++ jammit-watch.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'fssm'
+
+class JammitWatch
+  def self.compile(base, relative)
+	`jammit -c jammit.yml -o public/js/assets`
+  end
+  
+  def self.monitor
+    paths = {'public/js' => '*.js'}
+
+	FSSM.monitor do |monitor|
+	  paths.each do |path, glob|		
+	    monitor.path path do |path|
+	      path.glob glob
+	      
+          path.update { |base, relative| 
+            puts "Change detected in #{base}/#{relative}"
+            JammitWatch.compile(base, relative)
+          }	      
+	    end
+	  end
+	end
+  end
+end
+
+if $0 == __FILE__
+  JammitWatch.monitor
+end
